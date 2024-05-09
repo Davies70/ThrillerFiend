d3d7cb53a398506d7acd5e0b016ecd02a5e8abaa
ajayi daves <ajayid89@gmail.com>
2024-05-09 22:13:59 +0200

    chore: Remove commented out code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import Layout from './components/Layout';
 
 function App() {
-  // const URI = 'https://www.googleapis.com/books/v1';
-  // const KEY = 'AIzaSyC-p9UR2MYRUH4ibfNJanhjCjoxa6WLJ2A';
-
-  // const url = `${URI}/volumes?q=subject:thriller&key=` + KEY;
-  // fetch(url)
-  //   .then((response) => {
-  //     if (!response.ok) {
-  //       throw new Error('Network response was not ok');
-  //     }
-  //     return response.json();
-  //   })
-  //   .then((data) => {
-  //     console.log(data);
-  //     // Handle the response data here
-  //   })
-  //   .catch((error) => {
-  //     console.error('There was a problem with the fetch operation:', error);
-  //   });
-
+  
   return (
     <>
       <Layout><div>children</div></Layout>
