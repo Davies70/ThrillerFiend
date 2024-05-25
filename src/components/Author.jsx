9b88dcc77d31e2ede5ee898921b6e5e864e12975
ajayi daves <ajayid89@gmail.com>
2024-05-25 11:54:37 +0200

    chore: Update header and content scroller styles

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import '../styles/Author.css';
 import Proptypes from 'prop-types';
-
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
 const Author = ({ authorName, coverPhoto }) => {
   return (
-    <div className='author'>
-      <div>
-        <a>
+    <div className='card'>
+      <div className='card-content'>
+        <div className='imageWrapper'>
           <img src={coverPhoto} loading='lazy' alt={authorName} />
-        </a>
-      </div>
-      <div>
-        <a>{authorName}</a>
+        </div>
+        <div className='bg'>
+          <button className='left-button' tabIndex='-1'>
+            <ArrowCircleRightOutlinedIcon />
+          </button>
+          <button className='right-button'>
+            <MoreHorizOutlinedIcon />
+          </button>
+        </div>
+
+        <div className='tag'>
+          <a className='taglink'>{authorName}</a>
+        </div>
       </div>
     </div>
   );
